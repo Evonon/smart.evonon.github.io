8bd7f18c7b18bdcb5f514d1eaef1fa517c0a9cc5
Evonon <64852743+Evonon@users.noreply.github.com>
2020-06-02 16:01:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.menu-btn').on('click', function(e) {
+  e.preventDefault();
+  $(this).toggleClass('menu-btn_active');
+  $('.menu-nav').toggleClass('menu-nav_active');
+});
+
+$('.contact-btn').on('click', function(e) {
+  e.preventDefault();
+});
+
+$('.modern-btn').on('click', function(e) {
+  e.preventDefault();
+});
+
+$('.kockoi-btn').on('click', function(e) {
+  e.preventDefault();
+});
